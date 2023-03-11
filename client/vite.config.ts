a0c54b4d32868b7c5ea21a13ad0cdb915836a56d
Estrella <9123869+strlla@user.noreply.gitee.com>
2023-03-11 16:00:40 +0800

    npm 基础配置

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        // naive ui 的自动引入，只需要这一句
+        NaiveUiResolver()
+        // AntDesign vue 的自动引入，只需要这一句
+        // AntDesignVueResolver()
+      ]
+    }),],
+
 })
